299592ee550f29482fd8fa0f15a06a4f8722463b
LuisFuentesDev <lsfuentes2@gmail.com>
2023-06-19 21:09:45 -0400

    Ahora funcion el botón show Toast

--- app/src/main/java/com/example/contador/MainActivity.java
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.contador;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +16,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initListeners();
     }
-}
+
+        private void initListeners(){
+            Button showToast = findViewById(R.id.buttonMostrar);
+            Button counter = findViewById(R.id.buttonContar);
+            TextView textView = findViewById(R.id.textView);
+            showToast.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getBaseContext(), "hello toast", Toast.LENGTH_SHORT).show();
+                }
+            });
+            counter.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                   int numero = Integer.getInteger(textView.getText().toString());
+                   numero++;
+                   textView.setText(numero);
+                }
+            });
+        }
+    }
